ac1b771645f7152de75490f45c31a25c59c78245
Adam Vu <gvu0110@gmail.com>
2021-06-01 22:40:48 -0400

    Added unit test for domain access token

--- domain/access_token/access_token_test.go
+++ domain/access_token/access_token_test.go
@@ -9,6 +9,8 @@ import (
 
 func TestAccessTokenConstants(t *testing.T) {
 	assert.EqualValues(t, 24, expirationTime, "Expiration time should be 24 hours")
+	assert.EqualValues(t, "password", grantTypePassword)
+	assert.EqualValues(t, "client_credentials", grantTypeClientCredentials)
 }
 
 func TestGetNewAccessToken(t *testing.T) {
@@ -24,4 +26,100 @@ func TestAccessTokenIsExpired(t *testing.T) {
 
 	at.Expires = time.Now().UTC().Add(3 * time.Hour).Unix()
 	assert.False(t, at.IsExpired(), "Access token expiring 3 hours from now should NOT be expired")
+}
+
+func TestValidateAccessTokenInvalidID(t *testing.T) {
+	at := AccessToken{
+		AccessToken: "",
+		UserID:      123,
+		ClientID:    234,
+		Expires:     345,
+	}
+	err := at.Validate()
+	assert.EqualValues(t, "Invalid access token ID", err.Message())
+	assert.EqualValues(t, 400, err.StatusCode())
+	assert.EqualValues(t, "bad_request", err.Error())
+}
+
+func TestValidateAccessTokenInvalidUserID(t *testing.T) {
+	at := AccessToken{
+		AccessToken: "123abc",
+		UserID:      -1,
+		ClientID:    234,
+		Expires:     345,
+	}
+	err := at.Validate()
+	assert.EqualValues(t, "Invalid user ID", err.Message())
+	assert.EqualValues(t, 400, err.StatusCode())
+	assert.EqualValues(t, "bad_request", err.Error())
+}
+
+func TestValidateAccessTokenInvalidClientID(t *testing.T) {
+	at := AccessToken{
+		AccessToken: "123abc",
+		UserID:      123,
+		ClientID:    -1,
+		Expires:     345,
+	}
+	err := at.Validate()
+	assert.EqualValues(t, "Invalid client ID", err.Message())
+	assert.EqualValues(t, 400, err.StatusCode())
+	assert.EqualValues(t, "bad_request", err.Error())
+}
+
+func TestValidateAccessTokenInvalidExpirationTime(t *testing.T) {
+	at := AccessToken{
+		AccessToken: "123abc",
+		UserID:      123,
+		ClientID:    234,
+		Expires:     -1,
+	}
+	err := at.Validate()
+	assert.EqualValues(t, "Invalid expiration time", err.Message())
+	assert.EqualValues(t, 400, err.StatusCode())
+	assert.EqualValues(t, "bad_request", err.Error())
+}
+
+func TestValidateAccessTokenNoError(t *testing.T) {
+	at := AccessToken{
+		AccessToken: "123abc",
+		UserID:      123,
+		ClientID:    234,
+		Expires:     345,
+	}
+	err := at.Validate()
+	assert.Nil(t, err)
+}
+
+func TestValidateAccessTokenRequestInvalidGrantType(t *testing.T) {
+	request := AccessTokenRequest{
+		GrantType: "abc",
+	}
+	err := request.Validate()
+	assert.EqualValues(t, "Invalid grant type parameter", err.Message())
+	assert.EqualValues(t, 400, err.StatusCode())
+	assert.EqualValues(t, "bad_request", err.Error())
+}
+
+func TestValidateAccessTokenRequestNoError(t *testing.T) {
+	request := AccessTokenRequest{
+		GrantType: grantTypePassword,
+	}
+	err := request.Validate()
+	assert.Nil(t, err)
+
+	request = AccessTokenRequest{
+		GrantType: grantTypeClientCredentials,
+	}
+	err = request.Validate()
+	assert.Nil(t, err)
+}
+
+func TestGenerate(t *testing.T) {
+	at := AccessToken{
+		UserID:  123,
+		Expires: 345,
+	}
+	at.Generate()
+	assert.EqualValues(t, "0f21432ecbf12034c99350948c7a0726", at.AccessToken)
 }
